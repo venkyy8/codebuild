d103648e8af3ce025528d9851e1ec1bd864324e7
venkat <113894770+venkyy8@users.noreply.github.com>
2023-08-02 16:44:42 +0530

    vs commit

---
+++ scripts/start_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull venkyy82/venky:latest
+
+# Run the Docker image as a container
+docker run -d -p 5000:5000 venkyy82/venky:latest
