00c1193b7d2b2597b648e052d87b6bab730d3d3e
venkat <113894770+venkyy8@users.noreply.github.com>
2023-08-02 16:24:33 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull venkyy82/venky:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 venkyy82/venky:latest
